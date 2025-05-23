1bfabd93b793e0df0d1f9e888ca6cc8d8d66ce58
Wolf <sybilwolf@proton.me>
2025-05-22 18:43:23 -0700

    Code cleanup

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ import glob
 URS_ROOT_DIR = "../URS"
 URS_SCRAPES_RELATIVE_DIR = f"./scrapes"
 FINAL_OUTPUT_DIR = "../eth-daily-archiver-data/data"
-THREAD_ID_REGEX = re.compile('/comments/(.+?)/')
-SUBREDDIT_ID_REGEX = re.compile('r/(.+?)/comments/')
+THREAD_ID_REGEX = re.compile(r'/comments/(.+?)/')
+SUBREDDIT_ID_REGEX = re.compile(r'r/(.+?)/comments/')
+POST_ID_FROM_FILENAME_REGEX = re.compile(r'-([a-zA-Z0-9]+)\.json$')
 
 # Argument parsing
 parser = argparse.ArgumentParser(description="Reddit daily scraper")
@@ -35,8 +36,8 @@ if args.check_quota:
     sys.exit(0)
 
 # Create directories if they don't exist
-subprocess.check_output(f"mkdir -p {FINAL_OUTPUT_DIR}/", shell=True, universal_newlines=True)
-subprocess.check_output(f"mkdir -p {URS_SCRAPES_RELATIVE_DIR}/", shell=True, universal_newlines=True)
+os.makedirs(FINAL_OUTPUT_DIR, exist_ok=True)
+os.makedirs(URS_SCRAPES_RELATIVE_DIR, exist_ok=True)
 
 # Create a list of all scrapes from dailies.json
 # GitHub URL:
@@ -54,7 +55,8 @@ with urllib.request.urlopen(doots_json_url) as response:
 three_days_ago = time.time() - (3 * 24 * 60 * 60)
 all_scrapes_json = [elem for elem in all_scrapes_json if time.mktime(time.strptime(elem['date'], "%Y-%m-%d")) < three_days_ago]
 
-# Transform the all_scrapes_json to a dictionary with the thread id as the key and the date as the value
+# Transform the all_scrapes_json to a dictionary with the thread id as the key and the official dailydoots thread date as the value
+# This is used later to add the thread date metadata to the thread that has just been scraped
 all_scrapes_dict = {}
 for elem in all_scrapes_json:
     match = THREAD_ID_REGEX.search(elem['link'])
@@ -62,22 +64,18 @@ for elem in all_scrapes_json:
         thread_id = match[1]
         all_scrapes_dict[thread_id] = elem['date']
 
-# Create a list of finished scrape IDs
-finished_scrape_filename_list = subprocess.check_output(f"find {FINAL_OUTPUT_DIR}/ -type f -name '*.json'", shell=True, universal_newlines=True).splitlines()
+# Create a list of already finished scrape IDs
+finished_scrape_filename_list = glob.glob(f"{FINAL_OUTPUT_DIR}/**/*.json", recursive=True)
 print(f"Found {len(finished_scrape_filename_list)} postprocessed scrape files. Generating list of files to scrape...")
 finished_scrape_id_list = []
-
 for line in finished_scrape_filename_list:
-    with open(line) as json_data:
-        finished_scrape = json.load(json_data)
-        link = finished_scrape['urs_data']['scrape_settings']['url']
-        thread_id = THREAD_ID_REGEX.search(link)[1]
+    match = POST_ID_FROM_FILENAME_REGEX.search(os.path.basename(line))
+    if match:
+        thread_id = match[1]
         finished_scrape_id_list.append(thread_id)
 
-# List diff
+# Create upcoming_scrapes_json: only scrape threads whose id is not already in finished_scrape_id_list
 finished_scrape_id_set = set(finished_scrape_id_list)
-
-# Create upcoming_scrapes_json: only scrapes whose id is not in finished_scrape_id_list
 upcoming_scrapes_json = []
 for elem in all_scrapes_json:
     match = THREAD_ID_REGEX.search(elem['link'])
@@ -86,48 +84,68 @@ for elem in all_scrapes_json:
         if thread_id not in finished_scrape_id_set:
             upcoming_scrapes_json.append(elem)
 
-# Print statistics
+# Print statistics to the user before we begin scraping
 total_ct = len(all_scrapes_json)
 finished_ct = len(finished_scrape_id_list)
 upcoming_ct = len(upcoming_scrapes_json)
 print(f'Total Daily Threads: {total_ct}, In Archive: {finished_ct}, To Archive: {upcoming_ct}')
 
-# Process up to X reddit threads
+# Process up to X reddit threads in this invocation of main.py, based on the command line option
 if args.number_of_discussions == 0:
     num_discussions = len(upcoming_scrapes_json)
 else:
     num_discussions = args.number_of_discussions
 
-def run_urs_and_get_requests(urs_call, urs_root_dir):
-    process = subprocess.Popen(urs_call, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=f"{urs_root_dir}/", universal_newlines=True)
-    output_lines = []
-    for line in process.stdout:
-        # print(line, end='')
-        output_lines.append(line)
-    process.stdout.close()
-    return_code = process.wait()
-    output_str = ''.join(output_lines)
-    if return_code != 0:
-        raise subprocess.CalledProcessError(return_code, urs_call)
-    req_match = re.search(r"\|\s*(\d+)\s*\|\s*(\d+)\s*\|", output_str)
-    if req_match:
-        remaining_requests = int(req_match.group(1))
-        used_requests = int(req_match.group(2))
-        print(f"Remaining Requests: {remaining_requests}, Used Requests: {used_requests}")
-        return remaining_requests, used_requests
-    else:
-        print("Error: Could not extract Remaining and Used Requests from output.")
-        sys.exit(1)
+def get_all_json_files():
+    print("Listing all JSON files in the URS scrapes directory...")
+    scrape_dir = f"{URS_ROOT_DIR}/{URS_SCRAPES_RELATIVE_DIR}/**/*.json"
+    print(f"Glob: {scrape_dir}")
+    globbed_files = set(glob.glob(scrape_dir, recursive=True))
+    print(f"Globbed files: {len(globbed_files)}")
+    return set(globbed_files)
 
-def postprocess_scrape(filepath):
-    print(f"Postprocessing: {filepath}")
+for i in range(min(num_discussions, len(upcoming_scrapes_json))):
+    thread = upcoming_scrapes_json[i]
+    print()
+    print(f"Now Scraping: date={thread['date']}, comments={thread['comments']}")
+
+    # 1. List all json files before scraping
+    before_files = get_all_json_files()
+
+    # 2. Run URS
+    urs_call = f"poetry run python ./Urs.py -c {thread['link']} 0"
+    print(f"> {urs_call}")
+    result = subprocess.run(
+        urs_call,
+        shell=True,
+        cwd=f"{URS_ROOT_DIR}/urs/",
+        capture_output=True,
+        text=True
+    )
+    if result.returncode != 0:
+        print(result.stdout)
+        raise subprocess.CalledProcessError(result.returncode, urs_call)
+
+    # 3. List all json files after scraping
+    after_files = get_all_json_files()
+
+    # 4. Find the new file(s)
+    new_files = list(after_files - before_files)
+    if not new_files:
+        print("Warn: No new JSON file found after scraping.")
+        continue
+    # If multiple, pick the newest
+    newest_file_name = max(new_files, key=os.path.getmtime)
+
+    # 5. Postprocess the new scrape file, adding some metadata and renaming it
+    print(f"Postprocessing: {newest_file_name}")
     # Collect date metadata from the scrape
-    with open(filepath) as json_data:
+    with open(newest_file_name) as json_data:
         original_json_object = json.load(json_data)
         thread_link = original_json_object['scrape_settings']['url']
         thread_id = THREAD_ID_REGEX.search(thread_link)[1]
         subreddit_id = SUBREDDIT_ID_REGEX.search(thread_link)[1]
-    # Add the modified (retrieval) time to the json
+
     date_of_thread = all_scrapes_dict.get(thread_id, None)
 
     # Create the new structure
@@ -144,43 +162,12 @@ def postprocess_scrape(filepath):
         json.dump(new_json, outfile, separators=(',', ':')) # Minify JSON
 
     # Rename the original file to add the ".finished" suffix to indicate it has been processed
-    finished_filepath = filepath + ".finished"
-    print(f"Renaming original file to: {finished_filepath}")
-    os.rename(filepath, finished_filepath)
+    newest_file_postprocessed_name = newest_file_name + ".finished"
+    print(f"Renaming original file to: {newest_file_postprocessed_name}")
+    os.rename(newest_file_name, newest_file_postprocessed_name)
 
-def get_all_json_files():
-    print("Listing all JSON files in the URS scrapes directory...")
-    scrape_dir = f"{URS_ROOT_DIR}/{URS_SCRAPES_RELATIVE_DIR}/**/*.json"
-    print(f"Glob: {scrape_dir}")
-    globbed_files = set(glob.glob(scrape_dir, recursive=True))
-    print(f"Globbed files: {len(globbed_files)}")
-    return set(globbed_files)
-
-for i in range(min(num_discussions, len(upcoming_scrapes_json))):
-    thread = upcoming_scrapes_json[i]
-    print()
-    print(f"Now Scraping: date={thread['date']}, comments={thread['comments']}")
-    urs_call = f"poetry run python ./Urs.py -c {thread['link']} 0"
-    print(f"> {urs_call}")
-
-    # 1. List all json files before scraping
-    before_files = get_all_json_files()
-
-    # 2. Run URS
-    remaining_requests, used_requests = run_urs_and_get_requests(urs_call, URS_ROOT_DIR + "/urs/")
-
-    # 3. List all json files after scraping
-    after_files = get_all_json_files()
-
-    # 4. Find the new file(s)
-    new_files = list(after_files - before_files)
-    if not new_files:
-        print("Warn: No new JSON file found after scraping.")
-        continue
-    # If multiple, pick the newest
-    newest_file = max(new_files, key=os.path.getmtime)
-    postprocess_scrape(newest_file)
-    print(f"Upcoming scrapes left: {len(upcoming_scrapes_json) - i - 1}")
+    # 6. Continue scraping
+    print(f"Upcoming scrapes to perform this run: {len(upcoming_scrapes_json) - i - 1}")
     print("Continuing...")
 
 print(f"Successfully completed ({num_discussions}) scrapes.")
